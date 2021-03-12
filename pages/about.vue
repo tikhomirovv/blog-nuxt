286ee6476e5cd074ee9fbd8e0a01cb6fc745dc7c
Valerii Tikhomirov <v.tikhomirov.dev@gmail.com>
2021-03-13 02:17:36 +0300

    Navbar, layout, static page, nuxt-link

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid assumenda id molestias pariatur porro rerum?
+      Beatae, cum cumque delectus dicta dolorum natus repellat sint. Dolores nulla quaerat quas?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet asperiores at laboriosam nesciunt, pariatur
+      quidem.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis dicta fugiat inventore optio temporibus
+      totam. Ab accusamus accusantium asperiores atque aut blanditiis consectetur cum, cumque debitis dolores ea eius
+      error excepturi facere illo in incidunt magnam maiores minima molestiae, mollitia necessitatibus nihil nobis
+      obcaecati placeat, possimus provident quam ratione recusandae repudiandae similique sit soluta tempora tempore vel
+      veniam voluptatem? Autem cum deleniti dignissimos excepturi facilis, fugiat modi molestias mollitia nobis nulla
+      officia quam repudiandae, voluptate. Aliquid consequatur delectus error excepturi exercitationem, inventore libero
+      nesciunt nostrum praesentium quidem repudiandae velit? Ab eos est in minima possimus quae qui, repudiandae
+      saepe?</p>
+  </section>
+</template>
